8f61bfefa2b8dbb31227153f058a14fb1b2ae6ed
Nick <hi@nkls.cc>
2017-04-11 21:27:18 +0200

    oauth

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-var env = require('./config.json'),
-    InsomBot = require('./insombot/index.js'),
-    Discord = require('discord.js');
+const env = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var ins = new InsomBot;
-var bot = new Discord.Client();
-
-bot.on("ready", function () {
-    console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+client.on('ready', () => {
+  console.log('I am ready!');
 });
 
-bot.on('message', function(msg)
-{
-    if (typeof ins.loadKeywords() !== 'undefined' && ins.loadKeywords().length > 0) {
-        ins.checkMessageForKeywords(msg.content, ins.loadKeywords(), function(keyword)
-        {
-            if (keyword != 0) {
-                ins.runKeywordFunction(ins.getKeyByValue(ins.keywords, keyword), keyword, msg, function(reply)
-                {
-                    bot.reply(msg, reply);
-                });
-            }
-        });
-    }
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
 });
 
-bot.on('disconnected', function () {
-    console.log('Disconnected.');
-    process.exit(1);
-});
-
-bot.login(env.discord.token);
+client.login(env.discord.token);
