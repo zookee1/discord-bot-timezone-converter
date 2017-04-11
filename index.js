f3210e09465fd2291d0cc31dc13926edc974f492
Nick <hi@nkls.cc>
2017-04-11 21:39:26 +0200

    oauth

--- index.js
+++ index.js
@@ -10,19 +10,10 @@ bot.on("ready", function () {
     console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
 });
 
-bot.on('message', function(msg)
-{
-    if (typeof ins.loadKeywords() !== 'undefined' && ins.loadKeywords().length > 0) {
-        ins.checkMessageForKeywords(msg.content, ins.loadKeywords(), function(keyword)
-        {
-            if (keyword != 0) {
-                ins.runKeywordFunction(ins.getKeyByValue(ins.keywords, keyword), keyword, msg, function(reply)
-                {
-                    bot.reply(msg, reply);
-                });
-            }
-        });
-    }
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
 });
 
 bot.on('disconnected', function () {
