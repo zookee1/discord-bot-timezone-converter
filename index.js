e6148d17573102eb8cf98c84c04f2938fbc57e08
Nick <hi@nkls.cc>
2017-04-11 23:44:33 +0200

    oauth

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on('message', message => {
         let timeToConvert   = splitTime(m[3]);
         let d = new Date(today.getFullYear(), today.getMonth(), today.getDate(), timeToConvert[0], timeToConvert[1], 0);
         let startTimeInTimezone = converter.tz((d), fromTimeZone);
-        let convertedTime = startTimeInTimezone.clone().tz(toTimeZone).format('LT');
+        let convertedTime = startTimeInTimezone.clone().tz('Europe/Berlin').format('LT');
 
         message.reply(convertedTime);
     }
