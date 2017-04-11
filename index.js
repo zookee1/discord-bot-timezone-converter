1173b7e3b4c247701baa9ea203ab289b8aa3fdee
Nick <hi@nkls.cc>
2017-04-11 23:13:08 +0200

    oauth

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ bot.on('message', message => {
         let d = new Date();
         let fromTimeZone    = m[1];
         let toTimeZone      = m[2];
-        let timeToConvert   = d.setHours(splitTime(m[3]));
+        console.log(splitTime(m[3]));
+        let timeToConvert   = d.setHours(splitTime(m[3])[0],splitTime(m[3])[1],0,0);
         console.log(timeToConvert);
         let startTimeInTimezone = converter.tz((d), fromTimeZone);
         let convertedTime = formatDate(startTimeInTimezone.clone().tz(toTimeZone).format());
@@ -49,5 +50,5 @@ function splitTime(string) {
 
     minutes = (typeof m[2] === 'undefined' ? 0 : m[1]);
 
-    return m[1]+','+minutes+',0,0';
+    return [m[1],minutes];
 }
