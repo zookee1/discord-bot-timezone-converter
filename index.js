443b0278601fe00bca786dda4079140811831760
Nick <hi@nkls.cc>
2017-04-11 22:44:50 +0200

    oauth

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.on('message', message => {
         let toTimeZone      = m[2];
         let timeToConvert   = m[3];
         let startTimeInTimezone = converter.tz(d.getTime(timeToConvert), fromTimeZone);
-        let convertedTime = d.getTime(startTimeInTimezone.clone().tz(toTimeZone)).toTimeString.replace(/.*(\d{2}:\d{2}).*/, "$1");
+        let convertedTime = d.getTime(startTimeInTimezone.clone().tz(toTimeZone)).toTimeString().replace(/.*(\d{2}:\d{2}).*/, "$1");
 
         message.reply(convertedTime);
     }
